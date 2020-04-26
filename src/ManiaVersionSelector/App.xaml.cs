915e05104d9769bd2232bb874f64ed0ff3d71762
Stefan Baumann <stefan-baumann@outlook.com>
2020-04-26 20:42:55 +0200

    Exception handling

--- src/ManiaVersionSelector/App.xaml.cs
+++ src/ManiaVersionSelector/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -16,51 +17,73 @@ namespace ManiaVersionSelector
     {
         private void ApplicationStartup(object sender, StartupEventArgs e)
         {
-            string configPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "configuration.json");
-            if (File.Exists(configPath))
+            try
             {
-                Configuration.Instance = Configuration.LoadFromFile(configPath);
-            }
-            else
-            {
-                new Configuration()
+                string configPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "configuration.json");
+                if (File.Exists(configPath))
                 {
-                    Versions = new List<VersionEntry>()
+                    try
+                    {
+                        Configuration.Instance = Configuration.LoadFromFile(configPath);
+                    }
+                    catch (JsonReaderException ex)
+                    {
+                        MessageBox.Show("Could not read configuration.json file.");
+                    }
+                }
+                else
+                {
+                    new Configuration()
+                    {
+                        Versions = new List<VersionEntry>()
                     {
                         new VersionEntry() { Name = "ManiaPlanet", Path="C:\\...\\ManiaPlanet.exe" },
                         new VersionEntry() { Name = "TrackMania United Forever", Path="C:\\...\\TrackMania United Forever.exe" },
                     }
-                }.SaveToFile(configPath);
-                MessageBox.Show("No configuation.json file found in the application directory. Created template file.");
-                //new ConfigurationEditorWindow().ShowDialog();
-            }
+                    }.SaveToFile(configPath);
+                    MessageBox.Show("No configuation.json file found in the application directory. Created template file.");
+                    //new ConfigurationEditorWindow().ShowDialog();
+                }
 
-            string[] args = e.Args;
+                string[] args = e.Args;
 #if DEBUG
             args = new[] { "C:\\TestFile.Map.Gbx" };
 #endif
-            if (args.Length == 1)
-            {
-                string path = args[0];
+                if (args.Length == 1)
+                {
+                    string path = args[0];
 #if DEBUG
                 if (true)
 #else
-                if (File.Exists(path))
+                    if (File.Exists(path))
 #endif
-                {
-                    if (path.ToLowerInvariant().EndsWith(".gbx"))
                     {
-                        new MainWindow(new ManiaVersionSelectorViewModel() { FilePath = path }).Show();
-                        return;
+                        if (path.ToLowerInvariant().EndsWith(".gbx"))
+                        {
+                            new MainWindow(new ManiaVersionSelectorViewModel() { FilePath = path }).Show();
+                            return;
+                        }
                     }
+
+                    MessageBox.Show("Unknown file type: " + path);
+                    this.Shutdown();
+                    return;
                 }
 
-                MessageBox.Show("Unknown file type: " + path);
+                MessageBox.Show("Unknown action.");
                 this.Shutdown();
-                return;
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Unhandled exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Shutdown();
+            }
+        }
 
-            MessageBox.Show("Unknown action.");
+        private void ApplicationDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), "Unhandled exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
             this.Shutdown();
         }
     }
